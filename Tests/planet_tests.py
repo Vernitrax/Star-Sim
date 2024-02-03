91d6f1bf2d70033147d9607b5b7e90a5ed51b758
Vernitrax <ty21@op.pl>
2024-02-03 19:30:27 +0100

    planet_tests unittests created

---
+++ Tests/planet_tests.py
@@ -0,0 +1,17 @@
+import unittest
+
+from Models.Planet.Planet import Planet
+
+
+class TestStar(unittest.TestCase):
+    def test_factory_method(self):
+        pass
+
+    def test_apply_modifier(self):
+        pass
+
+    def test_remove_modifier(self):
+        pass
+
+    def test_colonize(self):
+        pass
