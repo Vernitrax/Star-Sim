1dd71b6e6aef5fafc22f562c2a95c5117c4c51fe
Vernitrax <ty21@op.pl>
2024-02-08 20:05:26 +0100

    Resource field tests imlemented

--- Tests/resource_field_tests.py
+++ Tests/resource_field_tests.py
@@ -3,10 +3,43 @@ import unittest
 
 class TestPlanet(unittest.TestCase):
     def test_factory_method(self):
-        pass
+        import Models.ResourceField.ResourceFieldFactory as ResourceFactory
+        factory_list = (
+            ResourceFactory.BalancedResourceFieldFactory(),
+            ResourceFactory.RichResourceFieldFactory(),
+            ResourceFactory.SparseResourceFieldFactory()
+        )
+        for factory in factory_list:
+            resource_field = factory.factory_method()
+            try:
+                resource_field.start_mining()  # duck-typing test
+            except AttributeError:
+                self.fail('Resource Field needs to implement start_mining')
 
     def test_start_mining(self):
-        pass
+        import Models.ResourceField.ResourceFieldFactory as ResourceFactory
+        factory_list = (
+            ResourceFactory.BalancedResourceFieldFactory(),
+            ResourceFactory.RichResourceFieldFactory(),
+            ResourceFactory.SparseResourceFieldFactory()
+        )
+        for factory in factory_list:
+            resource_field = factory.factory_method()
+            self.assertTrue(not resource_field.is_mined)
+            resource_field.start_mining()
+            self.assertTrue(resource_field.is_mined)
 
     def test_stop_mining(self):
-        pass
+        import Models.ResourceField.ResourceFieldFactory as ResourceFactory
+        factory_list = (
+            ResourceFactory.BalancedResourceFieldFactory(),
+            ResourceFactory.RichResourceFieldFactory(),
+            ResourceFactory.SparseResourceFieldFactory()
+        )
+        for factory in factory_list:
+            resource_field = factory.factory_method()
+            self.assertTrue(not resource_field.is_mined)
+            resource_field.start_mining()
+            self.assertTrue(resource_field.is_mined)
+            resource_field.stop_mining()
+            self.assertTrue(not resource_field.is_mined)
