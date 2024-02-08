90971b73beeac12c2a98ad7f5ef394c327515082
Vernitrax <ty21@op.pl>
2024-02-08 19:37:03 +0100

    resource field tests created

---
+++ Tests/resource_field_tests.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestPlanet(unittest.TestCase):
+    def test_factory_method(self):
+        pass
+
+    def test_start_mining(self):
+        pass
+
+    def test_stop_mining(self):
+        pass
